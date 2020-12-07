a1604d988a6d94455aac367ec83e55f092d7c8cc
Royal Paschall <royal.paschall@gmail.com>
2020-12-07 13:33:18 -0500

    fix logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Logo from '../images/logo.svg';
 
 function Header(){
     return(
 <header className="header">
-			<img src={"../images/logo.svg"} className="logo" alt="Around US Logo" />
+			<img src={Logo} className="logo" alt="Around US Logo" />
 		</header>
     )}
 export default Header;
